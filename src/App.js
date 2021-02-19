b7fcc2ac3ca399056b87f0933a1cd6452e85bdba
Michael Winser <mike.winser@gmail.com>
2021-02-19 13:44:44 -0800

    website stub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar">
+        <div className="logo">LOGO</div>
+        <div className="menu">
+          <ul>
+            <li>Cart</li>
+            <li>Login</li>
+            <li>Create Account</li>
+          </ul>
+        </div>
+      </nav>
+    <div className="hero">
+      <h1>Spring is in the air!</h1>
+      <button>Shop Sales</button>
+    </div>
+    <div className="featured">
+      <ul>
+        <li>
+          <img></img>
+          <h3>Item Name</h3>
+          <h4>$50.00</h4>
+        </li>
+        <li>
+          <img></img>
+          <h3>Item Name</h3>
+          <h4>$50.00</h4>
+        </li>
+        <li>
+          <img></img>
+          <h3>Item Name</h3>
+          <h4>$50.00</h4>
+        </li>
+      </ul>
+    </div>
+    <footer>
+      <p>Copyright 2021</p>
+      <p>Social Media Icons</p>
+    </footer>
     </div>
   );
 }
