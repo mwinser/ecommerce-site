a294846fed15a944f1bba82aa9dee0a982e815b4
Michael Winser <mike.winser@gmail.com>
2021-02-22 08:50:25 -0800

    added search bar to browse page

--- src/components/AllItems.js
+++ src/components/AllItems.js
@@ -1,19 +1,34 @@
-import React, {useContext} from "react"
+import React, {useContext, useState} from "react"
 import Item from "./Item"
 import {Context} from "../Context"
 
 function AllItems () {
     const {allItems} = useContext(Context)
+    const [query, setQuery] = useState("")
+    let regex = new RegExp(query,'gi')
+
+    function queryChange(e){
+        setQuery(e.target.value)
+        regex = new RegExp(query,'gi')
+    }
 
     return (
+        <>
+        <label> Search:        
+        <input type="text" value={query} onChange={queryChange}></input>
+        </label>
         <ul>
-            {allItems.map(item => (
-                <li key={item.id}>
-                    <Item img={item}/>
-                </li>
-            )
+            {allItems
+                .filter(item=>regex.test(item.name)
+                    )
+                .map(item => (
+                    <li key={item.id}>
+                        <Item img={item}/>
+                    </li>
+                )
             )}
         </ul>
+        </>
     )
 }
 
