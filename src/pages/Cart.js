a05f812deb5247e9109c9f18ff32ef18d99f84b0
Michael Winser <mike.winser@gmail.com>
2021-02-22 08:26:17 -0800

    added order confirmation message to cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -5,15 +5,22 @@ import Item from "../components/Item"
 function Cart () {
     const {cartItems, clearCart} = useContext(Context)
     const isCartEmpty = cartItems.length===0
+
+
     function fakePurchase(e) {
+
         e.target.innerHTML = "Processing..."
         e.target.disabled = true
         setTimeout(()=>{
-        e.target.innerHTML = "Purchase Items"
-        e.target.disabled = false
-        clearCart()
+            e.target.innerHTML = "Purchase Items"
+            clearCart()
+            
+            const message = document.getElementById("cart-message")
+            message.innerHTML = "Order confirmed!"
+            setTimeout(()=>{
+                message.innerHTML = "Your cart is empty!"
+            },2000)
         }, 3000)
-        
     }
 
 
@@ -21,7 +28,7 @@ function Cart () {
         return (
             <ul className="cart-list">
                 {cartItems.map(item => (
-                <li><Item key={item.id} img={item}/></li>
+                <li key={item.id}><Item img={item}/></li>
             ))}
             </ul>
 
@@ -37,7 +44,7 @@ function Cart () {
         <>
             {isCartEmpty ? (
                 <div>
-                    <h1>Your cart is empty!</h1>
+                    <h1 id="cart-message">Your cart is empty!</h1>
                     <button disabled>Purchase Items</button>
                 </div>
                 ) : (
