863b18fa84d89a387ce00cc8cff3e042d57064d2
Michael Winser <mike.winser@gmail.com>
2021-02-23 08:52:49 -0800

    #items in cart in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 import React, {useContext} from "react"
 import {Link} from "react-router-dom"
-import {Context, ContextProvider} from "../Context"
+import {Context} from "../Context"
 
 function Navbar () {
-    const {user} = useContext(Context)
+    const {user, cartItems} = useContext(Context)
     return (
     <nav className="navbar">
         <div className="logo">.petals</div>
@@ -11,10 +11,11 @@ function Navbar () {
             <ul>
                 <Link to="/"><li>Home</li></Link>
                 <Link to="/browse"><li>Browse</li></Link>
-                <Link to="/cart"><li>Cart</li></Link>
+                <Link to="/cart"><li>{cartItems.length>0 ? `Cart(${cartItems.length})` : "Cart"}</li></Link>
                 <Link to="/login"><li>{user? user: "Login"}</li></Link>
             </ul>
         </div>
+        
     </nav>
     )
 }
